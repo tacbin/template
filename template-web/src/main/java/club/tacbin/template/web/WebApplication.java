ce07c47161dc81bcb2259d7b46ee7f9fb8119c27
tacbin <2571761894@qq.com>
2022-03-24 21:37:57 +0800

    pom update

--- template-web/src/main/java/club/tacbin/template/web/WebApplication.java
+++ template-web/src/main/java/club/tacbin/template/web/WebApplication.java
@@ -1,5 +1,6 @@
 package club.tacbin.template.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @SpringBootApplication
 @EnableSwagger2
+@MapperScan({"club.tacbin.login.mapper"})
 public class WebApplication {
 
     public static void main(String[] args) {
