d64664ffa259a734715653ae83b44fa5eb00036d
tacbin <2571761894@qq.com>
2022-09-05 09:05:06 +0800

    template update

--- template-service/src/main/java/club/tacbin/template/service/test/impl/TestServiceImpl.java
+++ template-service/src/main/java/club/tacbin/template/service/test/impl/TestServiceImpl.java
@@ -6,9 +6,9 @@ import org.apache.dubbo.config.annotation.DubboService;
 import org.apache.dubbo.greet.Greeter;
 import org.apache.dubbo.greet.HelloRequest;
 import org.apache.dubbo.greet.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-
-import java.util.concurrent.CompletableFuture;
 
 /**
  * @author Administrator
@@ -16,9 +16,13 @@ import java.util.concurrent.CompletableFuture;
 @DubboService(interfaceName = Greeter.JAVA_SERVICE_NAME)
 public class TestServiceImpl implements ITestService {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestServiceImpl.class);
 
     @Override
     public User sayHello(HelloRequest request) {
+        logger.info("info信息");
+        logger.warn("warn信息");
+        logger.error("error信息");
         return User.newBuilder().setAge(1).setName("tom").build();
     }
 
